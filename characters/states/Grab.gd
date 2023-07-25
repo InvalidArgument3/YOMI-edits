78484575eb5e279c3291355842e9885f55a4110d
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-07-25 01:00:29 -0500

    Initial commit

---
+++ characters/states/Grab.gd
@@ -0,0 +1,30 @@
+extends CharacterState
+
+onready var throw_box = $ThrowBox
+
+export  var forward_throw_state = "ForwardThrow"
+export  var back_throw_state = "BackThrow"
+export  var down_throw_state = "AirDownThrow"
+
+func _enter():
+	if data is Dictionary:
+		if data.x == 0 and data.y == 1:
+			throw_box.throw_state = down_throw_state
+		elif data.x == host.get_facing_int() and data.y == 0:
+			throw_box.throw_state = forward_throw_state
+		elif data.x == - host.get_facing_int() and data.y == 0:
+			throw_box.throw_state = back_throw_state
+
+func _frame_1():
+	throw_techable = true
+
+func _frame_9():
+	throw_techable = false
+
+func _tick():
+	host.apply_fric()
+	host.apply_grav()
+	host.apply_forces()
+	if started_in_air and air_type == AirType.Aerial:
+		if host.is_grounded():
+			queue_state_change("Landing", 6)
---
+++ characters/swordandgun/projectiles/TemporalRoundDefault.gd
@@ -0,0 +1,67 @@
+extends ObjectState
+
+const MUZZLE_FLASH_SCENE = preload("res://characters/swordandgun/projectiles/MuzzleFlash.tscn")
+onready var hitbox = $Hitbox
+
+var pos
+
+export  var fast = false
+
+func f1():
+	var diff = host.obj_local_center(host.creator.opponent)
+	host.distance = fixed.vec_len(str(diff.x), str(diff.y))
+	hitbox.damage = host.scale_damage(hitbox.damage)
+	hitbox.damage_in_combo = host.scale_damage(hitbox.damage_in_combo)
+	hitbox.minimum_damage = host.scale_damage(hitbox.minimum_damage)
+	hitbox.hitstun_ticks = host.scale_hitstun(hitbox.hitstun_ticks)
+	var facing = Utils.int_sign(diff.x)
+	if facing != 0:
+		host.set_facing(facing)
+
+func f2():
+	pos = host.obj_local_center(host.creator.opponent)
+	var dir = fixed.normalized_vec(str(pos.x), str(pos.y))
+	spawn_particle_relative(MUZZLE_FLASH_SCENE, Vector2(), Vector2(float(dir.x), float(dir.y)))
+	host.play_sound("Shoot")
+	host.play_sound("ShootBass")
+	host.sprite.hide()
+	hitbox.update_position(pos.x, pos.y)
+	hitbox.activate()
+
+func f3():
+	host.disable()
+	
+func _frame_17():
+	if not fast:
+		return 
+	f1()
+
+
+func _frame_18():
+	if not fast:
+		return 
+	f2()
+
+
+func _frame_19():
+	if not fast:
+		return 
+	f3()
+
+
+func _frame_50():
+	if fast:
+		return 
+	f1()
+
+
+func _frame_51():
+	if fast:
+		return 
+	f2()
+
+
+func _frame_52():
+	if fast:
+		return 
+	f3()
